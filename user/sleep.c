05faf09af47fe7e6727e31ecc3442995b1a8abf6
SimonTheMachine <76942056+SimonTheMachine@users.noreply.github.com>
2022-11-29 10:25:27 +0000

    testing of memory management

--- user/sleep.c
+++ user/sleep.c
@@ -1,5 +1,33 @@
 #include "kernel/types.h"
 #include "user/user.h"
+// Sleep is a malloc tester now
+#include "memory_management.h"
+
+int main()
+{
+
+    //Checks basic malloc expansion
+    int* array = (int*) _malloc(100 * sizeof(int));
+    int* array2 = (int*) _malloc(50 * sizeof(int));
+    int* array3 = (int*) _malloc(50 * sizeof(int));
+    int* array4 = (int*) _malloc(100 * sizeof(int));
+    printf("Done allocating");
+    //Checks if free function works (with the memory merge thing)
+    _free(array3);
+    _free(array2);
+    
+    //This is to check if the future proof memory merge works
+    _free(array4);
+    printf("Done freeing");
+    //Checks if it has merged them correctly
+    array2 = (int*) _malloc(170 * sizeof(int));
+    array3 = (int*) _malloc(10 * sizeof(int));
+    //Checks if it can allocate a new block even though some is free
+    int* array5 = (int*) _malloc(30 * sizeof(int));
+
+    printf("Done reallocating");
+}
+/*
 // function for usage informational message
 void usage()
 {
@@ -23,3 +51,6 @@ int main(int argc, char *argv[])
   // exit logging success state
   exit(0);
 }
+*/
+
+
